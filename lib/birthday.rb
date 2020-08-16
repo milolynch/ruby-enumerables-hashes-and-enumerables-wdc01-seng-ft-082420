c7c82f720176044cb8afa168ca95e8a97209e52f
beatific-chain-3778 <milobravman20@gmail.com>
2020-08-16 11:34:29 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,3 +7,10 @@
 def happy_birthday(birthday_kids)
   # add your code snippet here!
 end
+```ruby
+hash = {key1: "value1", key2: "value2"}
+
+hash.each do |key, value|
+  puts "#{key}: #{value}"
+end
+```
